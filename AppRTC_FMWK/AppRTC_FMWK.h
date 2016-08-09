0d8d8a80427c831829ad9f59f92e96ee86ea439e
zouxu <izouxv@gmail.com>
2016-08-09 14:50:27 +0800

    no message

---
+++ AppRTC_FMWK/AppRTC_FMWK.h
@@ -0,0 +1,19 @@
+//
+//  AppRTC_FMWK.h
+//  AppRTC_FMWK
+//
+//  Created by zouxu on 9/8/16.
+//  Copyright © 2016 team.bz.fishcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRTC_FMWK.
+FOUNDATION_EXPORT double AppRTC_FMWKVersionNumber;
+
+//! Project version string for AppRTC_FMWK.
+FOUNDATION_EXPORT const unsigned char AppRTC_FMWKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRTC_FMWK/PublicHeader.h>
+
+
