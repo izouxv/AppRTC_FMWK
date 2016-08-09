063bcadab3c2bc7f7c7727e1deb4be7a055020cd
zouxu <izouxv@gmail.com>
2016-08-09 15:20:23 +0800

    no message

--- AppRTC_FMWK/AppRTC_FMWK.h
+++ AppRTC_FMWK/AppRTC_FMWK.h
@@ -16,4 +16,64 @@ FOUNDATION_EXPORT const unsigned char AppRTC_FMWKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AppRTC_FMWK/PublicHeader.h>
 
+ 
+#import <AppRTC_FMWK/ARDAppClient.h>
+#import <AppRTC_FMWK/ARDMessageResponse.h>
+#import <AppRTC_FMWK/ARDRegisterResponse.h>
+#import <AppRTC_FMWK/ARDSignalingMessage.h>
+#import <AppRTC_FMWK/ARDUtilities.h>
+#import <AppRTC_FMWK/ARDWebSocketChannel.h>
+
+
+#import <AppRTC_FMWK/RTCAudioSource.h>
+#import <AppRTC_FMWK/RTCAudioTrack.h>
+#import <AppRTC_FMWK/RTCAVFoundationVideoSource.h>
+#import <AppRTC_FMWK/RTCDataChannel.h>
+#import <AppRTC_FMWK/RTCEAGLVideoView.h>
+#import <AppRTC_FMWK/RTCFileLogger.h>
+#import <AppRTC_FMWK/RTCI420Frame.h>
+#import <AppRTC_FMWK/RTCICECandidate.h>
+#import <AppRTC_FMWK/RTCICECandidate+JSON.h>
+#import <AppRTC_FMWK/RTCICEServer.h>
+#import <AppRTC_FMWK/RTCICEServer+JSON.h>
+#import <AppRTC_FMWK/RTCLogging.h>
+
+
+#import <AppRTC_FMWK/RTCMediaConstraints.h>
+#import <AppRTC_FMWK/RTCMediaConstraints+JSON.h>
+#import <AppRTC_FMWK/RTCMediaSource.h>
+#import <AppRTC_FMWK/RTCMediaStream.h>
+#import <AppRTC_FMWK/RTCMediaStreamTrack.h>
+#if !TARGET_OS_IPHONE
+#import <AppRTC_FMWK/RTCNSGLVideoView.h>
+#endif
+#import <AppRTC_FMWK/RTCOpenGLVideoRenderer.h>
+#import <AppRTC_FMWK/RTCPair.h>
+#import <AppRTC_FMWK/RTCPeerConnection.h>
+#import <AppRTC_FMWK/RTCPeerConnectionDelegate.h>
+#import <AppRTC_FMWK/RTCPeerConnectionFactory.h>
+#import <AppRTC_FMWK/RTCPeerConnectionInterface.h>
+#import <AppRTC_FMWK/RTCSessionDescription.h>
+#import <AppRTC_FMWK/RTCSessionDescription+JSON.h>
+#import <AppRTC_FMWK/RTCSessionDescriptionDelegate.h>
+#import <AppRTC_FMWK/RTCStatsDelegate.h>
+#import <AppRTC_FMWK/RTCStatsReport.h>
+#import <AppRTC_FMWK/RTCTypes.h>
+#import <AppRTC_FMWK/RTCVideoCapturer.h>
+#import <AppRTC_FMWK/RTCVideoRenderer.h>
+#import <AppRTC_FMWK/RTCVideoSource.h>
+#import <AppRTC_FMWK/RTCVideoTrack.h>
+
+//#import <AppRTC_FMWK/SocketRocket.h>
+#import <AppRTC_FMWK/SRWebSocket.h>
+
+
+
+
+
+
+
+
+
+
 
